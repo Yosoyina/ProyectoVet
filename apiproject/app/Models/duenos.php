16eac02b470a9ff0e92ca8d4a9a32db584238cb7
Carlos <131690161+Yosoyina@users.noreply.github.com>
2026-02-03 18:00:37 +0100

    Creacion de la tabla dueños, con sus campos y la relacion

--- apiproject/app/Models/duenos.php
+++ apiproject/app/Models/duenos.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class duenos extends Model
 {
-    //
+    use HasFactory;
+
+    //Tabla de la base de datos
+    protected $table = 'duenos';
+
+    // campos de la tabla
+    protected $fillable = [
+        'nombre',
+        'apellido',
+    ];
+
+    // Un dueño puede tener muchos animales
+    public function animales()
+    {
+        return $this->hasMany(Animales::class);
+    }
 }
